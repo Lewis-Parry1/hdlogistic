fc08898b3d668041af9f5c8134bd7fbc1bff5477
Lewis Parry <lewis.parry@warwick.ac.uk>
2026-07-16 17:47:28 +0100

    refactor: ruff format changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,29 +7,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'to-be-titled'
-copyright = '2026, Lewis Parry and Elizabeth White'
-author = 'Lewis Parry and Elizabeth White'
-release = '0.1'
+project = "to-be-titled"
+copyright = "2026, Lewis Parry and Elizabeth White"
+author = "Lewis Parry and Elizabeth White"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest', # 
-    'sphinx.ext.autodoc', # Automatically document functions and classes
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon', # Helps with parsing Numpy-style docstrings
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",  #
+    "sphinx.ext.autodoc",  # Automatically document functions and classes
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",  # Helps with parsing Numpy-style docstrings
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
