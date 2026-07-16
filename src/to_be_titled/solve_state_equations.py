485383daeda35d2c82a93a7943fde5312e2970c0
Elizabeth White <lizzywhite018@icloud.com>
2026-07-16 16:12:32 +0100

    feat: add no intercept/corrupt initial solver for state equations

---
+++ src/to_be_titled/solve_state_equations.py
@@ -0,0 +1,115 @@
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Any
+
+import numpy as np
+from numpy.typing import NDArray
+from scipy.optimize import minimize
+from scipy.special import roots_hermite
+
+from to_be_titled import state_equations
+
+
+def se_funcs(
+    kappa: float,
+    gamma: float,
+    alpha: float,
+    gh: tuple[NDArray[np.float64], NDArray[np.float64]],
+    prox_tol: float = 1e-10,
+    transform: bool = True,
+) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
+
+    def g(pars: NDArray[np.float64]) -> NDArray[np.float64]:
+        if transform:
+            # Prevents underflow/overflow when exponentiating
+            pars_clipped = np.clip(pars, -250, 250)
+            pars = np.exp(pars_clipped)
+
+        return state_equations._se0(
+            mu=pars[0],
+            b=pars[1],
+            sigma=pars[2],
+            kappa=kappa,
+            gamma=gamma,
+            alpha=alpha,
+            gh=gh,
+            prox_tol=prox_tol,
+        )
+
+    return g
+
+
+@dataclass
+class SolverResult:
+    solution: NDArray[np.float64]
+    func_value: NDArray[np.float64]
+    iterations: int
+    message: str
+    termination_code: int
+
+
+def init_solver(
+    kappa: float,
+    gamma: float,
+    alpha: float,
+    start: NDArray[np.float64],
+    init_method: str = "Nelder-Mead",
+    gh: tuple[NDArray[np.float64], NDArray[np.float64]] | None = None,
+    prox_tol: float = 1e-10,
+    **init_kwargs: Any,
+) -> SolverResult:
+    """
+
+    Initial solver to find warm start for main solver. The initial solver uses the
+    specified method to minimise the sum of squares of the residuals of the state
+    equations.
+
+    Parameters
+    ----------
+    kappa : float
+        asymptotic ratio of the columns/rows of the design matrix.
+        kappa should be in (0,1).
+    gamma : float
+        square root of signal strength
+    alpha : float
+        shrinkage parameter of the MDYPL estimator. alpha should be in (0,1).
+    start : NDArray[np.float64]
+        vector with starting values for `mu`, `b` and `sigma`.
+    init_method : str, optional
+        the method to be passed into scipy.minimize to minimize the sum of the
+        squared residuals, by default 'Nelder-Mead'
+    gh : tuple[NDArray[np.float64], NDArray[np.float64]] | None, optional
+        a tuple of gauss hermite quadrature nodes and weights as returned by
+        scipy.special.roots_hermite(), by default None in which case `gh`
+        is set to scipy.special.roots_hermite(200).
+    prox_tol : float, optional
+        tolerance for the computation of the proximal operator, by default 1e-10.
+
+    Returns
+    -------
+    SolverResult
+        Dataclass containing the solution, the residual vector at the solution,
+        the number of iterations, and the solver's convergence message/status.
+    """
+    if gh is None:
+        gh = roots_hermite(200)
+
+    g = se_funcs(kappa, gamma, alpha, gh=gh, prox_tol=prox_tol, transform=True)
+    start_log = np.asarray(np.log(start), dtype=np.float64)
+
+    def objective(pars_log: NDArray[np.float64]) -> float:
+        r = g(pars_log)
+        return float(np.dot(r, r))
+
+    # TODO: Lewis see comment needed for this to pass mypy test
+    res = minimize(objective, start_log, method=init_method, **init_kwargs)  # type: ignore[call-overload]
+
+    soln = np.exp(res.x)
+
+    return SolverResult(
+        solution=soln,
+        func_value=g(res.x),
+        iterations=res.nit,
+        message=res.message,
+        termination_code=res.status,
+    )
