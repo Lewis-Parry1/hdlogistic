d6d7eddb80dea014225a4136c925cf470bf92f46
Lewis Parry <lewis.parry@warwick.ac.uk>
2026-09-03 15:55:26 +0100

    wip

--- src/to_be_titled/summary.py
+++ src/to_be_titled/summary.py
@@ -107,14 +107,6 @@ class MDYPLSummary:
         """Check whether high-dimensional asymptotic corrections were applied."""
         return self.hd_diagnostics is not None
 
-    @property
-    def fittedvalues(self) -> FloatArray:
-        return self.linear_predictors
-
-    @property
-    def fittedprobs(self) -> FloatArray:
-        return self.fitted_probs
-
 
 def summary(
     result: MDYPLResults,
